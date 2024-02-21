22340484a790368ca0ccfd513f1a3fc561f170b4
SOUM SOMON <somon.soum9@gmail.com>
2024-02-21 23:07:25 +0700

    fix warning

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import {
   ApolloFederationDriverConfig,
 } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloServerPluginInlineTrace } from '@apollo/server/plugin/inlineTrace';
 import { join } from 'path';
 import { TestModule } from './test/test.module';
 
@@ -16,7 +17,10 @@ import { TestModule } from './test/test.module';
     GraphQLModule.forRoot<ApolloFederationDriverConfig>({
       driver: ApolloFederationDriver,
       playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      plugins: [
+        ApolloServerPluginInlineTrace(),
+        ApolloServerPluginLandingPageLocalDefault(),
+      ],
       autoSchemaFile: {
         federation: 2,
         path: join(process.cwd(), 'src/schema.gql'),
